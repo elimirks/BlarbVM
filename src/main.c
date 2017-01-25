ca41af230135a1fd0923c23ccab7e3b605b1d0d8
Elijah Mirecki <elimirks@gmail.com>
2017-01-24 19:13:20 -0500

    Added a debug flag.
    Before, debug output was always dumped. Now you have to specify --debug
    as an arg

--- src/main.c
+++ src/main.c
@@ -5,20 +5,40 @@
 #include "main.h"
 #include "vm.h"
 
+// Sorry for making this a global... it made the terminateVM function simpler :]
 BlarbVM *vm;
 
+void abortWithUsage(char *arg0) {
+    fprintf(stderr, "Usage: %s [--debug] path/to/code.blarb\n", arg0);
+    exit(1);
+}
+
 int main(int argc, char **argv) {
-	if (argc == 1) {
-		fprintf(stderr, "Usage: %s path/to/code.blarb\n", argv[0]);
-		return 1;
-	}
+    int debug = 0;
+
+    char *fileName = 0;
+    // Possibly load multiple files
+    for (int i = 1; i < argc; i++) {
+        if (strncmp(argv[i], "--debug", 7) == 0) {
+            debug = 1;
+        // Filename was already specified
+        } else if (fileName) {
+            abortWithUsage(argv[0]);
+        } else {
+            fileName = argv[i];
+        }
+    }
 
 	vm = BlarbVM_init();
+    if ( ! fileName) {
+        abortWithUsage(argv[0]);
+    }
 
-	BlarbVM_loadFile(vm, argv[1]);
-	BlarbVM_execute(vm);
-
-	BlarbVM_dumpDebug(vm);
+    BlarbVM_loadFile(vm, fileName);
+    BlarbVM_execute(vm);
+    if (debug) {
+        BlarbVM_dumpDebug(vm);
+    }
 	BlarbVM_destroy(vm);
 
 	return 0;
