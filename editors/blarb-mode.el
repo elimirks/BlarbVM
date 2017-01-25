fa382e4cc5dc0d247bd711134232ab9db7f9f936
Elijah Mirecki <elimirks@gmail.com>
2017-01-24 20:53:39 -0500

    Added an Emacs plugin for syntax highlighting

---
+++ editors/blarb-mode.el
@@ -0,0 +1,11 @@
+(require 'generic-x)
+
+(define-generic-mode 'blarb-mode
+  '(";")                            ;; comments start with ';'
+  nil
+  '(("#[a-zA-Z_][a-zA-Z0-9_]*" . 'font-lock-function-name-face)
+    ("[0-9]+" . font-lock-constant-face))
+  '("\\.blarb$")                      ;; files for which to activate this mode 
+  nil                               ;; other functions to call
+  "A mode for blarb files"          ;; doc string for this mode
+  )
